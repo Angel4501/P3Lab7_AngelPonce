14aa520f4629120bbadb83acb74050f7bef1e90b
unknown <adponce@unitec.edu>
2021-06-04 12:56:58 -0700

    Lab7

--- P3Lab7_AngelPonce.cpp
+++ P3Lab7_AngelPonce.cpp
@@ -2,7 +2,7 @@
 using namespace std;
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int menu();//funcion menu
+int menu();//funcion menu /////
 
 int main(int argc, char** argv) {
 	int a = 0;
